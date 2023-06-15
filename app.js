c4da9cdb673bc8576c5b2a75dab84b72bc37a61a
hasan <mmhotmail>
2023-06-15 12:45:49 +0300

    last commit

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ const container =  document.querySelector(".container");
 container.addEventListener("click", (e)=>{
 
     if (e.target.classList.contains("num")) {
+
+        h1.innerText == "0" ?  h1.innerText = e.target.innerText :
         h1.innerText += e.target.innerText
-        // console.log(e); 
+        
     }
     else if (e.target.classList.contains("op") && h1.innerText != "") { 
         pFirst.innerText = h1.innerText;
@@ -26,9 +28,47 @@ container.addEventListener("click", (e)=>{
         pSecond.innerText = "";
         h1.innerText = ""
     }
-    else if (e.target.innerText == "="){
- 
+    else if (e.target.innerText == "%" & h1.innerText != "") {
+        h1.innerText = +h1.innerText / 100;
+        pFirst.innerText = ""  
     }
+    else if (e.target.innerText == "="){
+        calculate()
+        pFirst.innerText = "";
+        pSecond.innerText = ""
+    }
+    else if (e.target.innerText == "."){
+        h1.innerText != "" ? h1.innerText+="." :  h1.innerText="0."
+
+    }
+    else if (e.target.innerText == "±" & h1.innerText != ""){
+        h1.innerText = h1.innerText * -1
+    }
+
 })
+
+// ******functions*****
+
+const calculate = ()=>{
+
+switch (pSecond.innerText) {
+    case "+":
+        h1.innerText  = +pFirst.innerText + +h1.innerText  ; 
+        break;
+    case "-":
+        h1.innerText  = +pFirst.innerText - +h1.innerText  ; 
+        break;
+    case "x":
+        h1.innerText  = pFirst.innerText * h1.innerText; 
+        break;
+    case "÷":
+        h1.innerText  = (+pFirst.innerText / +h1.innerText).toFixed(2); 
+        break;
+    case "%":
+        h1.innerText  = +pFirst.innerText / 100; 
+        break;
+}
+}
+
 
 
